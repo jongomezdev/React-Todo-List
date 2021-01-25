cd9ee370aa7b3d2cc27219399aa8c3564903c8d2
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-25 15:22:38 -0500

    Create css styles, add components folder with app.jsx

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello World!</h1>,
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
