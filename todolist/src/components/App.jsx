7c78600163fcb3bd7b03ff3a1ec686954d43dd66
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-25 15:30:49 -0500

    Create constant for inputText change

--- todolist/src/components/App.jsx
+++ todolist/src/components/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input type="text" value={inputText} />
         <button>
           <span>Add</span>
         </button>
