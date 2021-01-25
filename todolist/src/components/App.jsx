3645350534542289987d7ae111ccd889e154e7fe
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-25 15:46:14 -0500

    store input items in an array

--- todolist/src/components/App.jsx
+++ todolist/src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
     const newValue = event.target.value;
@@ -15,13 +16,15 @@ function App() {
       </div>
       <div className="form">
         <input onChange={handleChange} type="text" value={inputText} />
-        <button>
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
